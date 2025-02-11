0d208fa178c8207428fbd9952d8d5f0c179ba887
Amir Afshar <afshar_amir_ar@yahoo.com>
2025-02-11 13:46:29 +0330

    initial bridge class made

--- puzzle_handler.py
+++ puzzle_handler.py
@@ -1,5 +1,25 @@
 from PySide6.QtCore import QObject, Property, Signal, Slot
 import threading
+# import numpy as np
 
 class PuzzleBridge(QObject):
-    pass
+    puzzle_list_changed = Signal()
+    
+    def __init__(self):
+        super().__init__()
+        self.puzzle_list = []
+        t = threading.Thread(target=self.main_func, daemon=True)
+        t.start()
+
+    def set_puzzle_list(self, value):
+        self.puzzle_list = value
+        self.puzzle_list_changed.emit()
+        
+    def get_puzzle_list(self):
+        return self.puzzle_list
+    
+    pyside_puzzle_list = Property(list, set_puzzle_list, get_puzzle_list, notify=puzzle_list_changed)
+    
+    def main_func(self):
+        puzzle_3_3 = [1, 2, 3, 4, 0, 5, 6, 7, 8]
+        self.set_puzzle_list(puzzle_3_3)
